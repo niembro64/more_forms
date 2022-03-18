57184b4682669716da61587573662d862698da72
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-18 15:08:14 -0400

    form started

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>test eric</h1>
+        <h1>More Forms</h1>
+      <div className="appinner">
+        <div className="item">
+          <label for="first_name">First Name: </label>
+          <input type="text" name="first_name" />
+        </div>
+        <div className="item">
+          <label for="last_name">Last Name:</label>
+          <input type="text" name="last_name" />
+        </div>
+        <div className="item">
+          <label for="email">Email:</label>
+          <input type="text" name="last_name" />
+        </div>
+        <div className="item">
+          <label for="password">Password:</label>
+          <input type="text" name="password" />
+        </div>
+        <div className="item">
+          <label for="confirm_password">Confirm Password:</label>
+          <input type="text" name="confirm_password" />
+        </div>
+      </div>{" "}
     </div>
   );
 }
