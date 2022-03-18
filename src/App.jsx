1db0af8599134e2e5b9ecdb41830c845f30e818a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-18 15:25:39 -0400

    good css

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,40 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-        <h1>More Forms</h1>
-      <div className="appinner">
+      <h1>More Forms</h1>
+
+      <form className="appinner">
         <div className="item">
-          <label for="first_name">First Name: </label>
-          <input type="text" name="first_name" />
+          <label className="label" for="first_name">
+            First Name:{" "}
+          </label>
+          <input className="input" type="text" name="first_name" />
         </div>
         <div className="item">
-          <label for="last_name">Last Name:</label>
-          <input type="text" name="last_name" />
+          <label className="label" for="last_name">
+            Last Name:
+          </label>
+          <input className="input" type="text" name="last_name" />
         </div>
         <div className="item">
-          <label for="email">Email:</label>
-          <input type="text" name="last_name" />
+          <label className="label" for="email">
+            Email:
+          </label>
+          <input className="input" type="text" name="last_name" />
         </div>
         <div className="item">
-          <label for="password">Password:</label>
-          <input type="text" name="password" />
+          <label className="label" for="password">
+            Password:
+          </label>
+          <input className="input" type="text" name="password" />
         </div>
         <div className="item">
-          <label for="confirm_password">Confirm Password:</label>
-          <input type="text" name="confirm_password" />
+          <label className="label" for="confirm_password">
+            Confirm Password:
+          </label>
+          <input className="input" type="text" name="confirm_password" />
         </div>
-      </div>{" "}
+      </form>
     </div>
   );
 }
