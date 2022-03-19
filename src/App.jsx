f7d0c88c1bd0b2d7465ba6ae1ab14b2eac5e16a8
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-19 00:44:56 -0400

    matching passwords hook not matching reality but everything else ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [form, setForm] = useState({
-    first_name: "default",
-    last_name: "default",
-    email: "default",
-    password: "default",
-    confirm_password: "default",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    confirm_password: "",
   });
 
+  const [errors, setErrors] = useState({
+    first_name: "init",
+    last_name: "init",
+    email: "init",
+    password: "init",
+    confirm_password: "init",
+    pass_match: false,
+  });
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const newObject = form;
+
+    console.log(newObject);
+  };
   const onChangeHandler = (event) => {
     event.preventDefault();
     // console.log("I'm here eric")
@@ -21,12 +37,33 @@ function App() {
       [event.target.name]: event.target.value,
     };
     setForm(newState);
-  };
 
-  const onSubmitHandler = (event) => {};
+    //////////// errors
 
-  const lengthValidator = (input, n) => {
-    return input.length >= n ? true : false;
+    var newPassMatch = true;
+    var newErrorValue = "";
+    if (event.target.value.length < 5) {
+      newErrorValue = event.target.name + " must be at least 5 chars";
+      // console.log(`name ${event.target.name} value ${event.target.value}`);
+      // console.log(`ename ${newErrorName} evalue ${newErrorValue}`);
+    } else {
+      newErrorValue = "";
+    }
+    if (event.target.name === "password") {
+      console.log("in password checker");
+      (event.target.value == form.confirm_password ? newPassMatch = true : newPassMatch = false);
+    } else if (event.target.name === "confirm_password") {
+      console.log("in confirm_password checker");
+      (event.target.value == form.password ? newPassMatch = true : newPassMatch = false);
+    } else {
+      console.log("in somewher else");
+    }
+    const newStateErr = {
+      ...errors,
+      [event.target.name]: newErrorValue,
+      passMatch: newPassMatch,
+    };
+    setErrors(newStateErr);
   };
 
   return (
@@ -35,7 +72,7 @@ function App() {
 
       <form onSubmit={onSubmitHandler} className="appinner">
         <div className="item">
-          <label className="label" for="first_name" >
+          <label className="label" htmlFor="first_name">
             First Name:{" "}
           </label>
           <input
@@ -46,8 +83,13 @@ function App() {
             placeholder="First Name"
           />
         </div>
+        {errors.first_name === "" || errors.first_name === "init" ? (
+          ""
+        ) : (
+          <p className="alert-danger">{errors.first_name}</p>
+        )}
         <div className="item">
-          <label className="label" for="last_name">
+          <label className="label" htmlFor="last_name">
             Last Name:
           </label>
           <input
@@ -58,20 +100,30 @@ function App() {
             placeholder="Last Name"
           />
         </div>
+        {errors.last_name === "" || errors.last_name === "init" ? (
+          ""
+        ) : (
+          <p className="alert-danger">{errors.last_name}</p>
+        )}
         <div className="item">
-          <label className="label" for="email">
+          <label className="label" htmlFor="email">
             Email:
           </label>
           <input
             className="input"
             type="text"
-            name="last_name"
+            name="email"
             onChange={onChangeHandler}
             placeholder="Email"
           />
         </div>
+        {errors.email === "" || errors.email === "init" ? (
+          ""
+        ) : (
+          <p className="alert-danger">{errors.email}</p>
+        )}
         <div className="item">
-          <label className="label" for="password">
+          <label className="label" htmlFor="password">
             Password:
           </label>
           <input
@@ -82,8 +134,13 @@ function App() {
             placeholder="Password"
           />
         </div>
+        {errors.password === "" || errors.password === "init" ? (
+          ""
+        ) : (
+          <p className="alert-danger">{errors.password}</p>
+        )}
         <div className="item">
-          <label className="label" for="confirm_password">
+          <label className="label" htmlFor="confirm_password">
             Confirm Password:
           </label>
           <input
@@ -94,17 +151,48 @@ function App() {
             placeholder="Confirm Password"
           />
         </div>
-        <input
-          type="submit"
-          className="btn btn-primary btn-lg d-block mx-auto my-2"
-        />
+        {errors.confirm_password === "" ||
+        errors.confirm_password === "init" ? (
+          ""
+        ) : (
+          <p className="alert-danger">{errors.confirm_password}</p>
+        )}
+        {errors.first_name == "" &&
+        errors.last_name == "" &&
+        errors.email == "" ? (
+          <input
+            type="submit"
+            className="btn btn-primary btn-lg d-block mx-auto my-2"
+          />
+        ) : (
+          ""
+        )}
+        {errors.pass_match ? (
+          <p></p>
+        ) : (
+          <p className="alert-danger">Passwords Dont' Match</p>
+        )}
       </form>
-      <div id="repeated">
-        <p name="test">first_name: {form.first_name}</p>
-        <p name="test">last_name: {form.last_name}</p>
-        <p name="test">email: {form.email}</p>
-        <p name="test">password: {form.password}</p>
-        <p name="test">confirm_password: {form.confirm_password}</p>
+      <div id="bottom">
+        <div id="repeated">
+          <p name="test">first_name: {form.first_name}</p>
+          <p name="test">last_name: {form.last_name}</p>
+          <p name="test">email: {form.email}</p>
+          <p name="test">password: {form.password}</p>
+          <p name="test">confirm_password: {form.confirm_password}</p>
+        </div>
+        <div id="repeated">
+          <p name="test">error_first_name: {errors.first_name}</p>
+          <p name="test">error_last_name: {errors.last_name}</p>
+          <p name="test">error_email: {errors.email}</p>
+          <p name="test">error_password: {errors.password}</p>
+          <p name="test">error_confirm_password: {errors.confirm_password}</p>
+          {errors.pass_match ? (
+            <p name="test">MATCH</p>
+          ) : (
+            <p name="test">XXX NO MATCH XXX</p>
+          )}
+        </div>
       </div>
     </div>
   );
